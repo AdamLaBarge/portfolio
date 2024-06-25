dfdf3805cdac3496c07aa06e9f72516e53492cc8
AdamLaBarge <139151022+AdamLaBarge@users.noreply.github.com>
2024-06-25 13:43:57 -0500

    Admin updates

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,5 +1,2 @@
 class ContactForm < ApplicationRecord
-  validates :email, presence: true
-  validates :subject, presence: true
-  validates :message, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :contact_forms
   get 'home/index'
   get 'up' => 'rails/healh#show', as: :rails_health_check
