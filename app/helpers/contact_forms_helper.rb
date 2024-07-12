ed40496c8dab8da03b6648f12f3f5100d98172b8
AdamLaBarge <139151022+AdamLaBarge@users.noreply.github.com>
2024-07-12 07:47:24 -0500

    Feat: Added sorting to messages

--- app/helpers/contact_forms_helper.rb
+++ app/helpers/contact_forms_helper.rb
@@ -1,2 +1,12 @@
 module ContactFormsHelper
+  def sortable(column, header = nil)
+    allowed_columns = %w[created_at]
+    header ||= column
+    if allowed_columns.include?(column)
+      direction = column == params[:sort] && params[:direction] == 'asc' ? 'desc' : 'asc'
+      link_to header, sort: column, direction:
+    else
+      header
+    end
+  end
 end
